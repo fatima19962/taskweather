49855f0bb941264a02747c9b6e83c0294021b118
fatemeh1996 <afvajfatemeh@gmail.com>
2023-02-03 19:26:09 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -65,9 +65,24 @@ function displayWeatherCondition(response) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
+// function searchLocation(position) {
+//   let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+//   axios.get(apiUrl).then(displayWeather);
+// }
+
 function searchLocation(position) {
   let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayWeather);
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
